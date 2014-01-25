2d3afb631a295dbb3c275bfffca345c0348fa249
ryandoom <ryan.doom@webascender.com>
2014-01-25 17:26:01 -0500

    first release

---
+++ app/controllers/data_controller.rb
@@ -0,0 +1,28 @@
+class DataController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+
+  def index
+    render :text => logit(params) if params.present? && params.length > 2  #action & controller
+  end
+
+  def create
+    #curl --data "email=hello@world.com&id=xyzlmnop&list_id=12345689&name=whats up doc&misc1=misc1 data&mist2=mist 2 day" http://localhost:3000/data    
+    render :text => logit(params)    
+  end
+
+  def logit(params)
+    params.delete(:action)
+    params.delete(:controller)
+
+    double_optin = params[:double_optin].present? ? params[:double_optin] : false    
+
+    if params[:id].present? && params[:list_id].present? && params[:email].present?
+      gb = Gibbon::API.new(params[:id])
+      gb.lists.subscribe({:id => params[:list_id], :email => {:email => params[:email]}, :merge_vars => params, :double_optin => double_optin})
+    end
+    # if params.keys.count > 0
+    #   Payload.create!(:params => params.to_yaml)
+    # end
+    return params
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MailchimpPost::Application.config.secret_key_base = 'a33392b3703924a1694bd8ea6678b8a489a9d41990bf0508ac8da940b8aa29cc127498dc181fe357d452c0166ea68ab5e0cbb65f1cc8bb70b4fd971b117eff26'
